5106ba9fda661b9b62753959b2a963965cee4e03
kevin0988459 <kevin0988459@gmail.com>
2021-06-29 23:55:25 +0800

    latest

---
+++ script.js
@@ -0,0 +1,16 @@
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.innerHTML = Math.floor(progress * (end - start) + start);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+
+  const obj = document.getElementById("value1");
+  r = obj.innerHTML
+  animateValue(obj, 0, r, 1000);
